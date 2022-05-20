d29e84ea2d197b26adb9df3147c8bd6a9106e890
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-20 23:21:40 +0300

    modified:   README.md new file:   Task_25/Program.cs

---
+++ Task_25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+int degreeResult;
+
+int RaiseToADegree(int number, int degreeOfNumber)
+{
+    int degree = 1;
+    for (int i = 0; i < degreeOfNumber; i++)
+    {
+        degree *= number; 
+    }
+    return degree;
+}
+
+degreeResult = RaiseToADegree(a, b);
+Console.WriteLine("{0}, {1} -> {2:F6}", a, b, degreeResult);
