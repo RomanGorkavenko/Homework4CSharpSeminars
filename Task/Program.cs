3396091846059f9802b11ba3def8b94712f2fccf
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-20 21:56:16 +0300

    modified:   README.md new file:   Task/Program.cs

---
+++ Task/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Ведите текст, чтобы напечатать его в рамке:");
+string str = Console.ReadLine();
+
+void WriteTextInAFrame(string text)
+{
+    Console.Clear();
+    string strUpDown = "+";
+    string strCentre = "| " + text + " |";
+    for (int i = 0; i < text.Length + 2; i++)
+    {
+        strUpDown = strUpDown + '-';
+    }
+    strUpDown = strUpDown + '+';
+    Console.WriteLine(strUpDown);
+    Console.WriteLine(strCentre);
+    Console.WriteLine(strUpDown);
+}
+
+WriteTextInAFrame(str);
