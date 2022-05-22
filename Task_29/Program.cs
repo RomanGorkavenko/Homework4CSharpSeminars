f87e68ad65a79bd538b3ca7bcef6517e8689f80e
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-22 23:34:51 +0300

    modified:   Task_29/Program.cs

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -1,9 +1,41 @@
-﻿int sum = 0;
-int resNum = 2;
-int number = 12;
-for (int i = 0; i < resNum; i++)
+﻿Console.Write("Введите размер массива: ");
+string len = Console.ReadLine();
+
+void ExceptionHandling(string enterA)
 {
-    sum = sum + (number % 10);
-    number = number / 10;
+    bool yesInt = int.TryParse(enterA, out int result);
+
+    if (yesInt && result > 0)
+    {
+        int[] rndNum = new int[result];
+        FillArray(rndNum);
+        PrintArray(rndNum);
+    }
+    else
+    {
+        Console.WriteLine("{0} -> Некорректный ввод!", len);
+    }
 }
-Console.WriteLine(sum);
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    string str = "[";
+    foreach (int i in arr)
+    {
+        str += $", {i}";
+    }
+    str = str.Remove(1, 2);
+    str += "]";
+    Console.Write(str);
+}
+
+ExceptionHandling(len);
